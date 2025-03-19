a555aad54ee983ed1756541145ab2e9892ac2b67
Sirmrmagoo <Sirmrmagoo@gmail.com>
2025-03-19 14:35:46 -0700

    Added Guess The Number v1.1.0 Fixed As Many Bugs As I Could Find And Changed Some Dialog

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
 
     public static void main(String[] args) {
         game();
@@ -15,139 +15,137 @@ public class Main {
         int guesses = 0;
 
         Scanner playerInput = new Scanner(System.in);
+
         Random random = new Random();
 
 
-        System.out.println("Welcome To Guess The Number!\nYou Have To Guess The Number I Think of.\nChose Your Difficulty\n1. Easy (Guess 1-100, 5 Guesses) \n2. Medium (Guess 1-150, 4 Guesses)\n3. Hard (Guess 1-300, 2 Guesses)");
+        try {
 
-        int difficulty = playerInput.nextInt();
+            System.out.println("\nWelcome To Guess The Number!\n\nYou Have To Guess The Number That I Think of.\n\nChose Your Difficulty\n1. Easy (Guess 1-100, 5 Guesses) \n2. Medium (Guess 1-150, 4 Guesses)\n3. Hard (Guess 1-300, 2 Guesses)");
 
-
-        if (difficulty == 1) {
-            guesses += 5;
-
-            System.out.print("You Chose Easy");
-
-            int number = random.nextInt(100 + 1);
-
-            System.out.println("\nYour Guesses:" + guesses);
+            int difficulty = playerInput.nextInt();
 
 
-            for (int i = 0; i < guesses; i++) {
-                int guess = playerInput.nextInt();
+            if (difficulty == 1) {
+                guesses += 5;
 
-                if (guess > number) {
-                    System.out.println("Smaller Than That");
-                } else if (guess < number) {
-                    System.out.println("Bigger Than That");
-                }else if (guess == number) {
-                    System.out.println("CORRECT!");
+                System.out.println("You Chose Easy\nLet's Start!\n1");
 
-                    System.out.println("Try Again?\n 1. Y\n2. N");
-
-                    int yN = playerInput.nextInt();
-
-                    if (yN == 1){
-                        game();
-                    }
-                }
-
-            }
-
-            System.out.println("You Ran Out Of Guesses Try Again?\n 1. Y\n2. N");
-
-            int yN = playerInput.nextInt();
-
-            if (yN == 1){
-                game();
-            }
+                int number = random.nextInt(100 + 1);
 
 
+                for (int i = 0; i < guesses; i++) {
 
-        } else if (difficulty == 2) {
+                    System.out.print("Your Guess? : ");
 
-            System.out.print("You Chose Medium");
-
-            guesses += 4;
-
-            int number = random.nextInt(150 + 1);
-
-            System.out.println("\nYour Guesses:" + guesses);
-
-            for (int i = 0; i < guesses; i++) {
-                int guess = playerInput.nextInt();
-
-                if (guess > number) {
-                    System.out.println("Smaller Than That");
-                } else if (guess < number) {
-                    System.out.println("Bigger Than That");
-                }else if (guess == number) {
-                    System.out.println("CORRECT!");
-
-                    System.out.println("Try Again?\n1. Y\n2. N");
-
-                    int yN = playerInput.nextInt();
-
-                    if (yN == 1){
-                        game();
-                    }
-                }
-
-            }
-
-            System.out.println("You Ran Out Of Guesses Try Again?\n1. Y\n2. N");
-
-            int yN = playerInput.nextInt();
-
-            if (yN == 1){
-                game();
-            }
+                    int guess = playerInput.nextInt();
 
 
+                    if (guess > number) {
+                        System.out.println("Smaller Than That\n");
 
-        } else if (difficulty == 3) {
 
-            System.out.print("You Chose Hard");
-            guesses += 2;
+                    } else if (guess < number) {
+                        System.out.println("Bigger Than That\n");
 
-            int number = random.nextInt(300 + 1);
 
-            System.out.println("\nYour Guesses:" + guesses);
-
-            for (int i = 0; i < guesses; i++) {
-                int guess = playerInput.nextInt();
-
-                if (guess > number) {
-                    System.out.println("Smaller Than That");
-                } else if (guess < number) {
-                    System.out.println("Bigger Than That");
-                }else if (guess == number) {
-                    System.out.println("CORRECT!");
-
-                    System.out.println("Try Again?\n1. Y\n2. N");
-
-                    int yN = playerInput.nextInt();
-
-                    if (yN == 1){
-                        game();
+                    } else if (number == guess) {
+                        System.out.println("CORRECT!\n It Took You " + i + " Attempts!");
+                        retry();
                     }
 
                 }
 
+                retry();
             }
 
-            System.out.println("You Ran Out Of Guesses Try Again?\n1. Y\n2. N");
+            if (difficulty == 2) {
+                guesses += 4;
 
-            int yN = playerInput.nextInt();
+                System.out.println("You Chose Easy\nLet's Start!\n1");
 
-            if (yN == 1){
+                int number = random.nextInt(150 + 1);
+
+
+                for (int i = 0; i < guesses; i++) {
+
+                    System.out.print("Your Guess? : ");
+
+                    int guess = playerInput.nextInt();
+
+
+                    if (guess > number) {
+                        System.out.println("Smaller Than That\n");
+
+
+                    } else if (guess < number) {
+                        System.out.println("Bigger Than That\n");
+
+
+                    } else if (number == guess) {
+                        System.out.println("CORRECT!\n It Took You " + i + " Attempts!");
+                        retry();
+                    }
+
+                }
+
+                retry();
+            }
+
+            if (difficulty == 3) {
+                guesses += 2;
+
+                System.out.println("You Chose Easy\nLet's Start!\n1");
+
+                int number = random.nextInt(300 + 1);
+
+
+                for (int i = 0; i < guesses; i++) {
+
+                    System.out.print("Your Guess? : ");
+
+                    int guess = playerInput.nextInt();
+
+
+                    if (guess > number) {
+                        System.out.println("Smaller Than That\n");
+
+
+                    } else if (guess < number) {
+                        System.out.println("Bigger Than That\n");
+
+
+                    } else if (number == guess) {
+                        System.out.println("CORRECT!\n It Took You " + i + " Attempts!");
+                        retry();
+                    }
+
+                }
+
+                retry();
+            }
+
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+            retry();
+        }
+    }
+
+
+    private static void retry(){
+        Scanner playerInput = new Scanner(System.in);
+
+        System.out.println("Retry?\n1. Yes\n2. No");
+
+        int yN = playerInput.nextInt();
+
+        try {
+            if (yN == 1) {
                 game();
             }
-
-
-        } else {
-            System.out.println("Invalid Input, Try Again!\n\n\n\n\n\n\n\n\n\n");
-            game();
+        }catch (InputMismatchException e ) {
+            System.out.println("Invalid Input");
         }
     }
 }
